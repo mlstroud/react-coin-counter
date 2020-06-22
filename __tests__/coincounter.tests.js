09914d681eada2d04492562402018cd62f83adc9
Matt Stroud <lixingke06@gmail.com>
2020-06-22 13:36:07 -0700

    add tests for countClosure

--- __tests__/coincounter.tests.js
+++ __tests__/coincounter.tests.js
@@ -16,11 +16,17 @@ describe('coin counter', () => {
     expect(result.countRecursive(amount)).toEqual("not a valid entry");
   });
 
-  test("should correctly return the amount of dollar coins for a given amount", () => {
+  test("should correctly return the amount of quarters for a given amount", () => {
     const amount = 4.99;
     const result = new Counter();
 
     expect(result.countRecursive(amount)).toEqual([19, 2, 0, 4]);
+  });
+
+  test("should correctly return the amount of quarters for a given amount", () => {
+    const result = new Counter();
+    const quarters = result.countClosure(.10);
+    expect(quarters(4.99)).toEqual(49);
   });
 });
 
--- src/coincounter.js
+++ src/coincounter.js
@@ -7,58 +7,16 @@ export class Counter {
     if (i > 3) {
       return arr;
     }
-    // if (amount == 0) {
-    //   return 0
-
     else {
       arr[i] = Math.floor(amount / values[i]);
       const remainder = amount %= values[i];
       return this.countRecursive(remainder, i + 1, arr)
-      // return arr;
     }
+  }
 
+  countClosure(coinAmount) {
+    return (totalAmount) => {
+      return Math.floor(totalAmount / coinAmount);
+    }
   }
 }
-
-/*
-
- function countRecursive(totalCost, numCoins = [], counter = 0) {
-  const coinValues = [100, 25, 10, 5, 1];
-  if(counter >= 5) {
-    return numCoins;
-  } else {
-    numCoins[counter] = Math.floor(totalCost/coinValues[counter]);
-    const totalChange = totalCost % coinValues[counter];
-    return countRecursive(totalChange, numCoins, counter + 1);
-  }
-}
-*/
-
-
-//   countRecursive(amount, i = 0, arr = []) {
-//     const values = [.25, .10, .05, .01];
-//     if (amount > 0) {
-//       const count = amount / values[i]
-//       const newAmount = amount %= values[i]
-//       const newArr = arr.push(count)
-//       return this.countRecursive(newAmount, i, NewArr)
-//     } else {
-//       return this.countRecursive(amount, (i += 1), arr)
-//     }
-//     const totalChange = arr;
-//     return totalChange;
-//   }
-// }
-
-
-
-
-
-
-
-// const newAmount = amount * 100
-// if (newAmount > 0) {
-//   const count = newAmount / values[i]
-//   newAmount %= values[i]
-//   arr.push(count)
-// }
