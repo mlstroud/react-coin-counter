4603c06edf44331f02d4d20765e00bf9d9ac4940
Matt Stroud <lixingke06@gmail.com>
2020-06-22 13:11:34 -0700

    add test for recursion. Test pass.

--- __tests__/coincounter.tests.js
+++ __tests__/coincounter.tests.js
@@ -6,14 +6,21 @@ describe('coin counter', () => {
     const amount = 0;
     const result = new Counter()
 
-    expect(result.countRecursive(amount)).toEqual(0);
+    expect(result.countRecursive(amount)).toEqual("not a valid entry");
   });
 
   test("should correctly return -1 if the amount is not a number", () => {
     const amount = "apple";
     const result = new Counter();
 
-    expect(result.countRecursive(amount)).toEqual(-1);
+    expect(result.countRecursive(amount)).toEqual("not a valid entry");
+  });
+
+  test("should correctly return the amount of dollar coins for a given amount", () => {
+    const amount = 4.99;
+    const result = new Counter();
+
+    expect(result.countRecursive(amount)).toEqual([19, 2, 0, 4]);
   });
 });
 
